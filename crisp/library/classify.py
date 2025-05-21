e3d4cdab5d657fa80f778846c62bb4c8d423ed55
hernandezb3 <brittney.hernandez@uconn.edu>
2025-05-21 07:54:27 -0400

    add ollama

--- crisp/library/classify.py
+++ crisp/library/classify.py
@@ -1,21 +1,21 @@
-# %%
+
 import shutil
 import os
 
 import numpy as np
 import pandas as pd
 from openai import OpenAI
+from langchain_ollama import OllamaLLM
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-from crisp.library import secrets
+from crisp.library import secrets, start
 
+# openai
 OPENAI_API_KEY = secrets.OPENAI_API_KEY
-
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-
 def format_message_and_get_response(
-    prompt, text_to_classify, model="gpt-4o", temperature=0.00
+    prompt, text_to_classify, model=start.MODEL, temperature=0.00
 ):
     messages = prompt + [{"role": "user", "content": text_to_classify}]
 
@@ -26,6 +26,13 @@ def format_message_and_get_response(
     )
     cleaned_response = response.choices[0].message.content
     return cleaned_response
+
+# llama
+ollama_server_url = "http://localhost:11434"
+llm = OllamaLLM(model = start.MODEL, base_url = ollama_server_url)
+
+#input = [{"role": "user", "content": text_to_classify}]
+#response = llm.invoke(input)
 
 
 def create_binary_classification_from_response(response):
@@ -48,3 +55,5 @@ def print_and_save_metrics(human_codes, classifications):
     print(f"Recall: {recall}")
     print(f"F1: {f1}")
     return accuracy, precision, recall, f1
+
+
