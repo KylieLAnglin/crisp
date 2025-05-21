7ed8de71b95426b2753a45925074a9ceb3865a16
hernandezb3 <brittney.hernandez@uconn.edu>
2025-05-21 09:17:47 -0400

    update paths and models

--- crisp/library/start.py
+++ crisp/library/start.py
@@ -1,24 +1,38 @@
 # ------------------ UPDATE HERE ------------------
+# USER = "Kylie", "Brittney", "Claudia", "HPC"
+# PLATFORM = "openai", "llama"
+# CONCEPT = "mm", "ncb", "goodreads"
+# SAMPLE = True, False
 USER = "Kylie"
 PLATFORM = "openai"
 CONCEPT = "mm"
 SAMPLE = False
+
 # ------------------ PATHS ------------------
-
-
 if USER == "Kylie":
-    # Kylie
     CODE_DIR = "/Users/kla21002/crisp/crisp/"
     ONEDRIVE = "/Users/kla21002/Library/CloudStorage/OneDrive-UniversityofConnecticut/"
+if USER == "Brittney":
+    CODE_DIR = "/Users/brittneyhernandez/Documents/github/crisp/crisp/"
+    ONEDRIVE = "/Users/brittneyhernandez/Library/CloudStorage/OneDrive-UniversityofConnecticut/"
+if USER == "Claudia":
+    CODE_DIR = "add/path/to/crisp/crisp/repo/"
+    ONEDRIVE = "add/path/to/OneDrive/"
+if USER == "HPC":
+    CODE_DIR = "add/path/to/crisp/crisp/repo/"
+    ONEDRIVE = "add/path/to/OneDrive/"
 
 MAIN_DIR = ONEDRIVE + "crisp/"
 DATA_DIR = MAIN_DIR + "data/"
 RESULTS_DIR = MAIN_DIR + "results/"
 RAW_DIR = ONEDRIVE + "Materials for LLM/"
 
+
 # ------------------ MODEL ------------------
 if PLATFORM == "openai":
     MODEL = "gpt-4.1-2025-04-14"
+if PLATFORM == "llama":
+    MODEL = "llama3.2"
 
 # ------------------ OTHERS ------------------
 SEED = 123
