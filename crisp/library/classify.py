382ad9c625f228e617a5bfd0f1bd2516e50237b5
KylieLAnglin <kylie.anglin@uconn.edu>
2025-05-27 12:50:15 -0400

    remove num_predict

--- crisp/library/classify.py
+++ crisp/library/classify.py
@@ -76,7 +76,7 @@ def format_message_and_get_response(
             model=start.MODEL,
             base_url=ollama_server_url,
             temperature=temperature,
-            num_predict=20,
+            # num_predict=20,
             seed=start.SEED,
         )
         response = llm.invoke(messages)
