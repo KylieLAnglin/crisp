686930ca8c59a9a92faed91a728a4af6f2a49e5b
KylieLAnglin <kylie.anglin@uconn.edu>
2025-05-30 15:42:58 -0400

    add files to run all

--- crisp/run_all_hpc.py
+++ crisp/run_all_hpc.py
@@ -11,22 +11,22 @@ print(
 # ------------------ Define Scripts ------------------
 script_paths = [
     # 1. Baseline Prompt
-    "crisp/1_baseline_prompt/00_baseline_prep.py",
-    "crisp/1_baseline_prompt/01_baseline_train.py",
-    "crisp/1_baseline_prompt/02_baseline_dev.py",
-    "crisp/1_baseline_prompt/03_fewshot_train.py",
-    "crisp/1_baseline_prompt/04_fewshot_dev.py",
+    # "crisp/1_baseline_prompt/00_baseline_prep.py",
+    # "crisp/1_baseline_prompt/01_baseline_train.py",
+    # "crisp/1_baseline_prompt/02_baseline_dev.py",
+    # "crisp/1_baseline_prompt/03_fewshot_train.py",
+    # "crisp/1_baseline_prompt/04_fewshot_dev.py",
     # # # 2. Automatic Prompt Engineering
-    # "crisp/2_APE/01_APE_train.py",
-    # "crisp/2_APE/02_APE_dev.py",
-    # "crisp/2_APE/03_APE_fewshot_train.py",
-    # "crisp/2_APE/04_APE_fewshot_dev.py",
+    "crisp/2_APE/01_APE_train.py",
+    "crisp/2_APE/02_APE_dev.py",
+    "crisp/2_APE/03_APE_fewshot_train.py",
+    "crisp/2_APE/04_APE_fewshot_dev.py",
     # # 3. Persona
-    # "crisp/3_persona/01_persona_train.py",
-    # "crisp/3_persona/02_persona_dev.py",
-    # "crisp/3_persona/03_persona_fewshot_dev.py",
+    "crisp/3_persona/01_persona_train.py",
+    "crisp/3_persona/02_persona_dev.py",
+    "crisp/3_persona/03_persona_fewshot_dev.py",
     # # 4. Chain of Thought - Zero
-    # "crisp/4_cot/01_cot_zero_dev.py",
+    "crisp/4_cot/01_cot_zero_dev.py",
     # "crisp/4_cot/02_cot_fewshot_train.py",
     # "crisp/4_cot/03_cot_fewshot_dev.py",
     # # 5. Explaination
